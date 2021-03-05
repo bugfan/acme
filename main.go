40a9a825877942b425fd161c80f438098f09b833
bugfan <908958194@qq.com>
2021-03-05 18:45:06 +0800

    test

--- main.go
+++ main.go
@@ -5,28 +5,17 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"crypto/tls"
-	"time"
-	
 
 	"github.com/go-acme/lego/certcrypto"
 	"github.com/go-acme/lego/certificate"
-	"github.com/go-acme/lego/challenge/http01"
 	"github.com/go-acme/lego/challenge/tlsalpn01"
 	"github.com/go-acme/lego/lego"
 	"github.com/go-acme/lego/registration"
 )
-
-const PathPrefix = "/.well-known/acme-challenge"
-
-const LetsEncrypt = "https://acme-v02.api.letsencrypt.org/directory"
-
-// const LetsEncrypt = "https://acme-staging-v02.api.letsencrypt.org/directory"
 
 // You'll need a user or account type that implements acme.User
 type MyUser struct {
@@ -45,19 +34,7 @@ func (u *MyUser) GetPrivateKey() crypto.PrivateKey {
 	return u.key
 }
 
-
 func main() {
-	go func(){
-		p:=&HTTPSProxy{}
-		srv:=p.GetHTTPSServer(":443")
-		fmt.Println("https:",srv.ListenAndServeTLS("", ""))
-	}()
-	go func(){
-		p:=&HTTPProxy{}
-		srv:=p.GetHTTPServer(":80")
-		fmt.Println("http:",srv.ListenAndServe())
-	}()
-	time.Sleep(5e9)
 
 	// Create a user. New accounts need an email and private key to start.
 	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
@@ -66,14 +43,14 @@ func main() {
 	}
 
 	myUser := MyUser{
-		Email: "908958194@qq.com",
+		Email: "917719033@qq.com",
 		key:   privateKey,
 	}
 
 	config := lego.NewConfig(&myUser)
 
 	// This CA URL is configured for a local dev instance of Boulder running in Docker in a VM.
-	config.CADirURL = LetsEncrypt
+	config.CADirURL = "https://acme-v02.api.letsencrypt.org/directory"
 	config.Certificate.KeyType = certcrypto.RSA2048
 
 	// A client facilitates communication with the CA server.
@@ -86,15 +63,14 @@ func main() {
 	// because we aren't running as root and can't bind a listener to port 80 and 443
 	// (used later when we attempt to pass challenges). Keep in mind that you still
 	// need to proxy challenge traffic to port 5002 and 5001.
-
-	err = client.Challenge.SetHTTP01Provider(http01.NewProviderServer("", "5002"))
+	err = client.Challenge.SetHTTP01Provider(NewHTTP01Provider())
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = client.Challenge.SetTLSALPN01Provider(tlsalpn01.NewProviderServer("", "5001"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	// err = client.Challenge.SetTLSALPN01Provider(NewTLSALPN01Provider())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	// New users will need to register
 	reg, err := client.Registration.Register(registration.RegisterOptions{TermsOfServiceAgreed: true})
@@ -104,7 +80,7 @@ func main() {
 	myUser.Registration = reg
 
 	request := certificate.ObtainRequest{
-		Domains: []string{"a.flower53.cn"},
+		Domains: []string{"app.lt53.cn"},
 		Bundle:  true,
 	}
 	certificates, err := client.Certificate.Obtain(request)
@@ -116,77 +92,90 @@ func main() {
 	// private key, and a certificate URL. SAVE THESE TO DISK.
 	fmt.Printf("%#v\n", certificates)
 
-	<-chan int(nil)
 	// ... all done.
 }
 
-type HTTPProxy struct{}
-
-func  (p *HTTPProxy)ServeHTTP(w http.ResponseWriter,r *http.Request)  {
-	fmt.Println("ww1,http:",r.URL.String())
-	remote, err := url.Parse("http://127.0.0.1:5002")
-	if err != nil {
-		panic(err)
-	}
-	proxy := httputil.NewSingleHostReverseProxy(remote)
-	proxy.ServeHTTP(w, r)
+type HTTP01Provider struct {
+	tokens   map[string]string
+	keyAuths map[string]string
 }
-func (p *HTTPProxy)GetHTTPServer(listen string)*http.Server{
-	return &http.Server{
-		Handler:   p,
-		Addr:      listen,
+
+func NewHTTP01Provider() *HTTP01Provider {
+	return &HTTP01Provider{
+		tokens:   make(map[string]string),
+		keyAuths: make(map[string]string),
 	}
 }
 
-const ACMETLS1Protocol = "acme-tls/1"
-type HTTPSProxy struct{
-
+func (s *HTTP01Provider) Present(domain, token, keyAuth string) error {
+	s.tokens[domain] = token
+	s.keyAuths[domain] = keyAuth
+	return nil
 }
 
-func  (p *HTTPSProxy)ServeHTTP(w http.ResponseWriter,r *http.Request)  {
-	fmt.Println("ww2,https:",r.URL.String())
-	remote, err := url.Parse("http://127.0.0.1:5001")
-	if err != nil {
-		panic(err)
+func (s *HTTP01Provider) CleanUp(domain, token, keyAuth string) error {
+	if _, ok := s.tokens[domain]; ok {
+		delete(s.tokens, token)
 	}
-	proxy := httputil.NewSingleHostReverseProxy(remote)
-	proxy.ServeHTTP(w, r)
+
+	if _, ok := s.keyAuths[domain]; ok {
+		delete(s.keyAuths, token)
+	}
+	return nil
 }
 
+func (s *HTTP01Provider) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		token := s.tokens[r.Host]
+		if token != "" && r.URL.Path == ChallengePath(token) {
+			w.Header().Add("Content-Type", "text/plain")
+			w.Write([]byte(s.keyAuths[r.Host]))
+			return
 
-func (p *HTTPSProxy) GetHTTPSServer(listen string) *http.Server {
-	tlsconf := &tls.Config{
-		// GetCertificate: p.GetCertificate,
-		NextProtos:     []string{ACMETLS1Protocol},
-	}
-	tlsconf.InsecureSkipVerify = true
-	
-	// tlsconf.MaxVersion = tls.VersionTLS12
-	tlsconf.MinVersion = tls.VersionTLS12
-
-	tlsconf.PreferServerCipherSuites = true
-
-	tlsconf.CipherSuites = []uint16{
-		//tls.TLS_AES_128_GCM_SHA256,
-		//tls.TLS_CHACHA20_POLY1305_SHA256,
-		//tls.TLS_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+		}
 	}
 
-	return &http.Server{
-		Handler:   p,
-		TLSConfig: tlsconf,
-		Addr:      listen,
+	w.WriteHeader(http.StatusUnprocessableEntity)
+	w.Write([]byte("Unsupported"))
+}
+
+func ChallengePath(token string) string {
+	return "/.well-known/acme-challenge/" + token
+}
+
+type TLSALPN01Provider struct {
+	tokens   map[string]string
+	keyAuths map[string]string
+}
+
+func NewTLSALPN01Provider() *TLSALPN01Provider {
+	return &TLSALPN01Provider{
+		tokens:   make(map[string]string),
+		keyAuths: make(map[string]string),
 	}
+}
+
+func (s *TLSALPN01Provider) Present(domain, token, keyAuth string) error {
+	s.tokens[domain] = token
+	s.keyAuths[domain] = keyAuth
+	return nil
+}
+
+func (s *TLSALPN01Provider) CleanUp(domain, token, keyAuth string) error {
+	if _, ok := s.tokens[domain]; ok {
+		delete(s.tokens, domain)
+	}
+
+	if _, ok := s.keyAuths[domain]; ok {
+		delete(s.keyAuths, domain)
+	}
+	return nil
+}
+func (s *TLSALPN01Provider) GetCertificate(info *tls.ClientHelloInfo) (*tls.Certificate, error) {
+	for _, v := range info.SupportedProtos {
+		if k, has := s.keyAuths[info.ServerName]; has && v == "acme-tls/1" {
+			return tlsalpn01.ChallengeCert(info.ServerName, k)
+		}
+	}
+	return nil, nil
 }
